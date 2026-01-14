4948f97fe8305eb3da68e02118c41578c3deed6d
Jan Wieners <mail@jan-wieners.de>
2026-01-14 15:46:37 +0100

    Add API info.

--- src/environment.example.ts
+++ src/environment.example.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    ZAMMAD_API_TOKEN: '' as const,
-    BASE_URL: ''
+    ZAMMAD_API_TOKEN: 'YOUR_ZAMMAD_API_TOKEN' as const,
+    BASE_URL: 'YOUR_ZAMMAD_INSTANCE/api/v1' as const
 };
